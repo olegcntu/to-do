805c13bec93852d5a9935ed516d5f18935d77abb
oleg <oleg.krasavcik.41@gmail.com>
2023-07-03 15:40:06 +0300

    init-commot

---
+++ src/modals/AddEditTaskModal.js
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import crossIcon from "../assets/icon-cross.svg";
+import boardsSlice from "../redux/boardsSlice";
+
+function AddEditTaskModal({
+  type,
+  device,
+  setIsTaskModalOpen,
+  setIsAddTaskModalOpen,
+  taskIndex,
+  prevColIndex = 0,
+}) {
+  const dispatch = useDispatch();
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [isValid, setIsValid] = useState(true);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
+  const board = useSelector((state) => state.boards).find(
+    (board) => board.isActive
+  );
+
+  const columns = board.columns;
+  const col = columns.find((col, index) => index === prevColIndex);
+  const task = col ? col.tasks.find((task, index) => index === taskIndex) : [];
+  const [status, setStatus] = useState(columns[prevColIndex].name);
+  const [newColIndex, setNewColIndex] = useState(prevColIndex);
+
+
+
+  const onChangeStatus = (e) => {
+    setStatus(e.target.value);
+    setNewColIndex(e.target.selectedIndex);
+  };
+
+  const validate = () => {
+    setIsValid(false);
+    if (!title.trim()) {
+      return false;
+    }
+
+
+    setIsValid(true);
+    return true;
+  };
+
+  if (type === "edit" && isFirstLoad) {
+
+    setTitle(task.title);
+    setDescription(task.description);
+    setSelectedTime(task.selectedTime)
+    setIsFirstLoad(false);
+  }
+
+
+  const onSubmit = (type) => {
+    if (type === "add") {
+      dispatch(
+        boardsSlice.actions.addTask({
+          title,
+          description,
+          selectedTime,
+          status,
+          newColIndex,
+        })
+      );
+    } else {
+      dispatch(
+        boardsSlice.actions.editTask({
+          title,
+          description,
+          selectedTime,
+          status,
+          taskIndex,
+          prevColIndex,
+          newColIndex,
+        })
+      );
+    }
+  };
+
+  return (
+    <div
+      className={
+        device === "mobile"
+          ? "  py-6 px-6 pb-40  absolute overflow-y-scroll  left-0 flex  right-0 bottom-[-100vh] top-0 dropdown "
+          : "  py-6 px-6 pb-40  absolute overflow-y-scroll  left-0 flex  right-0 bottom-0 top-0 dropdown "
+      }
+      onClick={(e) => {
+        if (e.target !== e.currentTarget) {
+          return;
+        }
+        setIsAddTaskModalOpen(false);
+      }}
+    >
+
+      <div
+        className=" scrollbar-hide overflow-y-scroll max-h-[95vh]  my-auto  bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold
+       shadow-md shadow-[#364e7e1a] max-w-md mx-auto  w-full px-8  py-8 rounded-xl"
+      >
+        <h3 className=" text-lg ">
+          {type === "edit" ? "Edit" : "Add New"} Task
+        </h3>
+
+
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="  text-sm dark:text-white text-gray-500">
+            Task Name
+          </label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            id="task-name-input"
+            type="text"
+            className=" bg-transparent  px-4 py-2 outline-none focus:border-0 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#63c75f] outline-1  ring-0  "
+            placeholder=" e.g Take coffee break"
+          />
+        </div>
+
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="  text-sm dark:text-white text-gray-500">
+            Description
+          </label>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            id="task-description-input"
+            className=" bg-transparent outline-none min-h-[200px] focus:border-0 px-4 py-2 rounded-md text-sm  border-[0.5px] border-gray-600 focus:outline-[#63c75f] outline-[1px] "
+            placeholder="e.g. It's always good to take a break. This 
+            15 minute break will  recharge the batteries 
+            a little."
+          />
+        </div>
+        <div className="mt-8 flex flex-col space-y-1">
+          <label className="text-sm dark:text-white text-gray-500">
+            Deadline
+          </label>
+          <input
+              type="time"
+               value={selectedTime}
+              onChange={(e) => setSelectedTime(e.target.value)}
+              className="bg-transparent outline-none focus:border-0 px-4 py-2 rounded-md text-sm border-[0.5px] border-gray-600 focus:outline-[#63c75f] outline-[1px]"
+          />
+        </div>
+
+
+
+
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className="  text-sm dark:text-white text-gray-500">
+            Current Status
+          </label>
+          <select
+            value={status}
+            onChange={onChangeStatus}
+            className=" select-status flex-grow px-4 py-2 rounded-md text-sm bg-transparent focus:border-0  border-[1px] border-gray-300 focus:outline-[#63c75f] outline-none"
+          >
+            {columns.map((column, index) => (
+              <option key={index}>{column.name}</option>
+            ))}
+          </select>
+          <button
+            onClick={() => {
+              const isValid = validate();
+              if (isValid) {
+                onSubmit(type);
+                setIsAddTaskModalOpen(false);
+                type === "edit" && setIsTaskModalOpen(false);
+              }
+            }}
+            className=" w-full items-center text-white bg-[#63c75f] py-2 rounded-full "
+          >
+           {type === "edit" ? " save edit" : "Create task"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddEditTaskModal;
